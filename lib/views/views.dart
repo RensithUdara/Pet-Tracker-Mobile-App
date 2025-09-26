a1a91a939a22e7eaeb936ca1ee17579934d7e394
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-26 22:53:05 +0530

    Add views.dart file to export screens and widgets

---
+++ lib/views/views.dart
@@ -0,0 +1,10 @@
+// Screens
+export 'screens/welcome_screen.dart';
+export 'screens/login_screen.dart';
+export 'screens/sign_up_screen.dart';
+export 'screens/home_screen.dart';
+
+// Widgets
+export 'widgets/custom_button.dart';
+export 'widgets/custom_text_field.dart';
+export 'widgets/pet_logo.dart';
