9dc8e8dde9ab082642cdf4105e1c1d9341a57d01
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-26 22:50:06 +0530

    Add CustomTextField widget for enhanced text input with password toggle and validation

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_constants.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String labelText;
+  final String? hintText;
+  final bool obscureText;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool showPasswordToggle;
+  final VoidCallback? onTap;
+  final bool readOnly;
+
+  const CustomTextField({
+    super.key,
+    required this.labelText,
+    this.hintText,
+    this.obscureText = false,
+    this.controller,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.showPasswordToggle = false,
+    this.onTap,
+    this.readOnly = false,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.labelText,
+          style: AppTextStyles.body1.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: widget.controller,
+          obscureText: _obscureText,
+          validator: widget.validator,
+          keyboardType: widget.keyboardType,
+          onTap: widget.onTap,
+          readOnly: widget.readOnly,
+          style: AppTextStyles.body1,
+          decoration: InputDecoration(
+            hintText: widget.hintText ?? 'Enter your ${widget.labelText.toLowerCase()}',
+            hintStyle: AppTextStyles.body1.copyWith(
+              color: AppColors.textHint,
+            ),
+            prefixIcon: widget.prefixIcon,
+            suffixIcon: widget.showPasswordToggle
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: AppColors.textSecondary,
+                    ),
+                  )
+                : widget.suffixIcon,
+            filled: true,
+            fillColor: AppColors.surface,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+              borderSide: const BorderSide(color: AppColors.textHint, width: 1),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+              borderSide: const BorderSide(color: AppColors.textHint, width: 1),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+              borderSide: const BorderSide(color: AppColors.primary, width: 2),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+              borderSide: const BorderSide(color: AppColors.error, width: 1),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+              borderSide: const BorderSide(color: AppColors.error, width: 2),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: AppDimensions.paddingMedium,
+              vertical: AppDimensions.paddingMedium,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
