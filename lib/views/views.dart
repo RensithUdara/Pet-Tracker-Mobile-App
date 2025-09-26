69aa793a3b7652a50a4db11f96649e3d755b0d2b
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-26 22:53:10 +0530

    Swap home_screen and welcome_screen exports in views.dart

--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,9 +1,8 @@
 // Screens
-export 'screens/welcome_screen.dart';
+export 'screens/home_screen.dart';
 export 'screens/login_screen.dart';
 export 'screens/sign_up_screen.dart';
-export 'screens/home_screen.dart';
-
+export 'screens/welcome_screen.dart';
 // Widgets
 export 'widgets/custom_button.dart';
 export 'widgets/custom_text_field.dart';
